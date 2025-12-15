c8b5e7b6f9c2aa319163fa3c29ef60982945c5a2
Markus Lindell <markusgunnarvalo.lindell@utb.ecutbildning.se>
2025-12-15 09:01:53 +0100

    cleaned code

--- src/main.py
+++ src/main.py
@@ -1,13 +1,5 @@
-import sql_handler as sqlh
-
 def main():
     print("Main started successfully!")
-
-    print("Connecting...")
-    sqlh.Connect()
-
-    print("Connection established!")
-
 
 if __name__ == "__main__":
     main()

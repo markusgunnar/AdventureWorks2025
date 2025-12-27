0b9a6c93d5ab537d0743701aaa906462fdbe071d
Markus Lindell <markusgunnarvalo.lindell@utb.ecutbildning.se>
2025-12-27 13:10:08 +0100

    Finished reworked task 1

--- src/panda_helpers.py
+++ src/panda_helpers.py
@@ -4,7 +4,7 @@ def df_to_dict(_pd: pd.DataFrame) -> dict:
     df = pd.DataFrame()
 
     return df
-
+"""
 id_filter = [1, 3, 5]
 t = pd.DataFrame({
     "ID": [1, 2, 3, 4, 5],
@@ -19,3 +19,13 @@ print(under_30)
 
 t_age = t.loc[(t["Name"].str.startswith("T")) & (t["Age"] >= 25)]
 print(t_age)
+"""
+
+def get_unique_sort(_df: pd.DataFrame, _str: str) -> pd.DataFrame:
+    new_df = _df[_str].unique()
+    new_df.sort()
+    return new_df
+
+def sort_dict(_dict: dict, _reverse: bool=True) -> dict:
+    sorted_dict = {k: v for k, v in sorted(_dict.items(), key=lambda item: item[1], reverse=_reverse)}
+    return sorted_dict
